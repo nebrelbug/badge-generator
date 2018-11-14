5a596a8aee976c6e35ec717fd1dbf553dfa0241b
nebrelbug <nebrelbug@gmail.com>
2018-11-13 21:28:22 -0700

    Simplified the helper function

--- index.js
+++ index.js
@@ -7,35 +7,25 @@ let server = app.listen(8080, function () {
     console.log("listening to requests on port 8080");
 });
 
-Sqrl.defineHelper("returnColor", function (args, content, blocks) {
-    var colorScheme = {
+Sqrl.defineHelper("returnColor", function (arg) { // In our case, the argument will be the value of 'options.color'
+    var colorScheme = { // This just maps the written colors to hex codes. Try editing or adding more colors!
         brightgreen: "#4c1",
         green: "#97CA00",
-        yellow: "#dfb317",
-        yellowgreen: "#a4a61d",
         orange: "#fe7d37",
         red: "#e05d44",
         blue: "#007ec6",
-        grey: "#555",
-        gray: "#555",
-        lightgrey: "#9f9f9f",
-        lightgray: "#9f9f9f"
     }
-    if (colorScheme.hasOwnProperty(args)) {
-        console.log('That color is valid!')
-        var result = colorScheme[args]
+    if (colorScheme.hasOwnProperty(arg)) { // Then the color has a valid hex code associated with it
+        var result = colorScheme[arg] // Get the hex code
         console.log("Resulting Hex: " + result)
-        return result
-    } else {
-        if (args) {
-            return args
-        } else {
-            return "#4c1" // Default color of brightgreen
-        }
+        return result // Return the hex code
+    } else { // If the color doesn't have a valid hex code associated with it
+        return arg // Just in case it's one of the built-in SVG colors
     }
 })
 
 app.get('/:left/:right/:color', function (req, res) {
+    console.log(JSON.stringify(req.params))
     var badge = Sqrl.renderFile(path.join(__dirname, 'template.svg'), req.params)
     res.writeHead(200, {
         "Content-Type": "image/svg+xml"
